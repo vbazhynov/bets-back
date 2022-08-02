0a7b52c2522cc27c2f93156c5a6c7359510b2351
vbazhynov <vladbazhynov@gmail.com>
2022-08-02 12:18:59 +0300

    fix path

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express, { Router } from "express";
 import knex from "knex";
-import knexfile from "knexfile";
 import jwt from "jsonwebtoken";
 import joi from "joi";
 import ee from "events";
-import { dbConfig } from "./1.js";
 import routes from "./api/routes/index.js";
 
 const app = express();
@@ -21,7 +19,7 @@ const stats = {
 export let db;
 app.use(express.json());
 app.use((uselessRequest, uselessResponse, neededNext) => {
-  db = knex(knexfile.development);
+  db = knex(dbConfig.development);
   db.raw("select 1+1 as result")
     .then(function () {
       neededNext();
